e338f8a8ae50c6e0b768580e9d7e5e7992360313
Matthew Vallance <matthew.vallance@outlook.com>
2024-02-20 14:08:50 +0000

    Data generation and output script complete

--- data_generation.R
+++ data_generation.R
@@ -2,3 +2,28 @@
 # Date: 13/02/24
 # Description: Task 1 - Data generation
 
+# <------------------------- Task 1 ------------------------->
+# Weights of 200 mice before: mean = 20, and variance = 2
+mice_before <- rnorm(200, 20, sd = sqrt(2))
+
+# Weights of 200 mice after: mean = 21, and variance = 2.5
+mice_after <- rnorm(200, 21, sd = sqrt(2.5))
+
+# Weights of 200 rats before: shape = 10, and scale = 20
+rats_before <- rweibull(n = 200, 10, 20)
+
+# Weights of 200 rats after: shape = 9 and scale = 21
+rats_after <- rweibull(n = 200, 9, 21)
+
+print("Data generated")
+
+# Export all values to CSV, for data consistency in other tasks
+df_to_export <- data.frame(
+  mice_before = mice_before,
+  mice_after = mice_after,
+  rats_before = rats_before,
+  rats_after = rats_after
+)
+
+write.csv(df_to_export, "data_generation_output/data.csv", row.names = TRUE)
+print("Data written to CSV")
